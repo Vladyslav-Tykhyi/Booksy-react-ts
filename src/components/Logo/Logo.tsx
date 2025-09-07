ddbd5c382db6689ec9754063523568597cc38117
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2025-09-07 16:38:36 +0200

    fix logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import s from "./Logo.module.css";
 const Logo = () => {
   return (
     <a href="/" className={s.logo}>
-      <img src="/public/logo/svg" alt="Logo Booksy_shop" />
+      <img src="/public/logo/svg/Company_Logo.svg" alt="Logo Booksy_shop" />
     </a>
   );
 };
